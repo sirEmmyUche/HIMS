52b1c4ce8f1e179b418e93abd208b9324969490b
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-06-12 10:53:18 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div>
       <Link to={"/Login"}>This is the home page</Link>
+      <img src="https://10web-site.ai/36/wp-content/uploads/sites/39/2023/04/logoipsum-218-1_a19KCKOD.webp"/>
         
     </div>
    
