395a7c18d535f6df450c547fc1c598face2040e4
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-06-15 20:49:30 +0100

    updating home functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import {Link} from "react-router-dom"
+import Nav from '../pages/nav.jsx'
 
 
 function Home() {
   return (
-    <div>
-      <Link to={"/Login"}>This is the home page</Link>
-      <img src="https://10web-site.ai/36/wp-content/uploads/sites/39/2023/04/logoipsum-218-1_a19KCKOD.webp"/>
-        
-    </div>
+    <headers>
+      <Nav/>
+    </headers>
    
   )
 }
